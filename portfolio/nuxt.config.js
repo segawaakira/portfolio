c9ccfe3f0a40322da98a290dbb5a1649acc1d9c0
segawa <>
2019-08-25 23:07:18 +0900

    メタ情報修正

--- portfolio/nuxt.config.js
+++ portfolio/nuxt.config.js
@@ -1,15 +1,16 @@
-
+require('dotenv').config()
+const authorName = process.env.AUTHOR_NAME
 export default {
   mode: 'spa',
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: authorName + 'のポートフォリオサイト' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: authorName + 'のポートフォリオサイト' || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
